02ad035f4242c3c4388baff8b5579919fe1b3afb
maryshtd <maryshtd@gmail.com>
2018-11-08 18:13:06 +0100

    alertify

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { longStackSupport } from 'q';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,25 +11,25 @@ import { longStackSupport } from 'q';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe(next => {
-      console.log('logged in succesfully');
+      this.alertify.success('logged in succesfully');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     }
-    // console.log(this.model);
+
     );
   }
 
     logout() {
       this.authService.userToken = null;
       localStorage.removeItem('token');
-      console.log('logged out');
+      this.alertify.message('logged out');
     }
 
     loggedIn() {
